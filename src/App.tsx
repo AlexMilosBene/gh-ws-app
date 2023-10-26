2d1ece6f2f7d0e8f84e918f5eae1465ecbbc8480
AlexMilosBene <alex.milos@benestudio.co>
2023-10-26 16:33:27 +0200

    replaced with correct files

---
+++ src/App.tsx
@@ -0,0 +1,16 @@
+import React from 'react';
+import logo from './logo.svg';
+import './App.css';
+import { Heading } from '@shummy1991/package-demo';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <Heading>Github Workshop</Heading>
+      </header>
+    </div>
+  );
+}
+
+export default App;
--- src/Components/Heading/Heading.tsx
+++
@@ -1,7 +0,0 @@
-import { PropsWithChildren } from 'react';
-
-const Heading = ({ children }: PropsWithChildren) => (
-  <h1>{children}</h1>
-);
-
-export default Heading;
